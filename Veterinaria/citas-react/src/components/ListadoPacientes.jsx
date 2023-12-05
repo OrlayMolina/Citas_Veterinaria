06335797424587ba423992cd2c5d84f61a0ed5dd
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-05 18:11:35 -0500

    Se crea useState para poder editar un paciente en la página.

--- Veterinaria/citas-react/src/components/ListadoPacientes.jsx
+++ Veterinaria/citas-react/src/components/ListadoPacientes.jsx
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({pacientes, setPaciente}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      { pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
 
-      <p className="text-xl mt-5 mb-5 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-
-      {pacientes.map(paciente => (
-        <Paciente key={paciente.id} paciente={paciente} />
-      ))}
+          <p className="text-xl mt-5 mb-5 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+    
+          {pacientes.map(paciente => (
+            <Paciente key={paciente.id} paciente={paciente} setPaciente={setPaciente}/>
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No hay Pacientes</h2>
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Comienza Agendando Pacientes {''}
+            <span className='text-indigo-600 font-bold'>y aparecerán en este lugar.</span>
+          </p>
+        </>
+      )}
 
 
     </div>
@@ -22,6 +34,7 @@ const ListadoPacientes = ({pacientes}) => {
 
 ListadoPacientes.propTypes = {
   pacientes: PropTypes.array.isRequired,
+  setPaciente: PropTypes.func.isRequired,
 
 };
 
--- Veterinaria/citas-react/src/components/Paciente.jsx
+++ Veterinaria/citas-react/src/components/Paciente.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Paciente = ({paciente}) => {
+const Paciente = ({paciente, setPaciente}) => {
   const {nombre, propietario, email, fecha, sintomas} = paciente;
   return (
     <div className="mx-5 bg-white shadow-md px-5 py-10 rounded-lg mb-10">
@@ -23,12 +23,31 @@ const Paciente = ({paciente}) => {
         <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
           <span className="font-normal normal-case">{sintomas}</span>
         </p>
+
+        <div className='flex justify-between mt-6'>
+          <button 
+            type='button' 
+            className='py-2 px-10 bg-indigo-600 text-white font-bold uppercase hover:bg-indigo-700 rounded-lg'
+            onClick={ ( ) => setPaciente(paciente) }
+          >Editar
+          </button>
+            
+
+          <button 
+            type='button' 
+            className='py-2 px-10 bg-red-600 text-white font-bold uppercase hover:bg-red-700 rounded-lg'
+            
+          >Eliminar
+          </button>
+        </div>
+
       </div>
   )
 }
 
 Paciente.propTypes = {
   paciente: PropTypes.object.isRequired,
+  setPaciente: PropTypes.func.isRequired,
 };
 
 export default Paciente
